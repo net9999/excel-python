25f35a9ccd40571a7aa73e011e99b36eb3cc3741
net9999 <60805264+net9999@users.noreply.github.com>
2020-02-13 16:02:00 +0900

    Update excel_sheet_get02.py
    csvファイルに出力できるようにしました．

--- excel_sheet_get02.py
+++ excel_sheet_get02.py
@@ -1,12 +1,13 @@
 import openpyxl as px
 import os
+import csv
 
 #ファイルリストを読み込む
 fi = os.listdir()
 
-#出力する空の配列を作成
+#出力する空の配列1，2を作成
 li = []
-
+li2 = []
 #ファイルリストの要素について処理を行う
 for y in fi:
     #ドットを使ってスプリット
@@ -18,13 +19,20 @@ for y in fi:
         #ファイル名をリストの先頭に入れる
         li.append(y)
 
-        #ファイルを開きシート名を取得して，配列に追加する
+        #ファイルを開きシート名を取得して，配列1に追加する
         bk = px.load_workbook(y)
         sn = bk.sheetnames
         li.extend(sn)
 
-        #配列を出力
-        print(li)
 
-        #配列を空にする
+        #配列1を出力する配列2に追加
+        li2.append(li)
+
+        #for内の配列を空にする
         li = []
+
+#print(li2)
+#csvファイルを開き，保存，SHIFT-JISじゃないとうまくいかなかった
+with open("stock.csv", "w", encoding="SHIFT-JIS") as f:
+    writer = csv.writer(f,lineterminator='\n')
+    writer.writerows(li2)
